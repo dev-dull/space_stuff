741a66b09e3d132fec328975b84b11cff27901ba
Alastair Drong <alastair.drong@infogroup.com>
2020-09-27 14:53:02 -0700

    modify my size

--- spacestuff/Asteroid.java
+++ spacestuff/Asteroid.java
@@ -1,4 +1,25 @@
 package spacestuff;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Asteroid extends Particle
 {
+    public Asteroid()
+    {
+        size = ThreadLocalRandom.current().nextInt(10, 530000);
+    }
 }
+
+/*
+public class Particle
+{
+    private int gravity = 0;  // How much it will attract objects around itself that will grow its size
+    private double size = 1;
+    private int health = -1;  // Can't be destroyed
+
+    public double get_size()
+    {
+        return size;
+    }
+}
+*/
