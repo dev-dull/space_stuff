7875168a5a9659e5649fec9af10b053534b7517a
Alastair Drong <alastair.drong@infogroup.com>
2020-09-27 13:55:23 -0700

    Created custom java package for space stuff, made sure it all compiled.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import spacestuff.Particle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from the Main class main method!");
--- spacestuff/Particle.java
+++ spacestuff/Particle.java
@@ -1,10 +1,11 @@
-class Particle
+package spacestuff;
+public class Particle
 {
     private int gravity = 0;  // How much it will attract objects around itself that will grow its size
     private double size = 1;
     private int health = -1;  // Can't be destroyed
 
-    public static double get_size()
+    public double get_size()
     {
         return size;
     }
